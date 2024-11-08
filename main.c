cb47a6ae009021e5b03af54b7758e29887084945
ErfanKhebrati <khebrati@gmail.com>
2024-11-08 17:10:48 +0330

    Add fake malloc implementation: memalloc. Add logging

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void* lemalloc(size_t size, const char *file, int line, const char *func)
+{
+    void *p = malloc(size);
+    printf ("Allocated: File: %s, Line: %i, Func: %s, Memory: %p (%li bytes)\n", file, line, func,p,size);
+
+    return p;
+}
+
+#define malloc(X) lemalloc( X, __FILE__, __LINE__, __FUNCTION__)
+
+
+int main(int argc, char* arguments[]){
+    int* ip = (int*)malloc(sizeof(int));
+    *ip = 3;
+    printf("number is: %d\n",*ip);
+}
